2206b13143418ca0935a454258e0c95aec09f8d1
Victor Mota <victor2018mota@gmail.com>
2022-04-01 18:04:22 -0300

    create mobile app

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Projeto Integrado - Servy</Text>
+      <Text>Página em construção...</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 30,
+  }
+});
