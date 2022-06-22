0da7d07cfe6c5bbc7a2485a8f9522ed64a8fcb94
Eduardo <luiz.dudu3@gmail.com>
2022-06-22 15:43:07 -0300

    populate aula ao listar usuario

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -77,7 +77,7 @@ class UserController extends CrudController<IUser, typeof User> {
 
   override async posRead(user: IUser): Promise<void> {
     const roleField = user.role === 'teacher' || user.role === 'admin' ? 'teachers' : 'students'
-    const turmas = await Turma.find({[roleField]: user._id}).populate("disciplina").exec()
+    const turmas = await Turma.find({[roleField]: user._id}).populate("disciplina").populate("aulas").exec()
     user._doc.turmas = turmas
   }
  
