5709a6602cbf15d1fcee0451d70ddad3d9f87e99
Eduardo <luiz.dudu3@gmail.com>
2022-07-02 13:19:14 -0300

    refactor weekboard

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Router from './routes/routes';
 import "./App.css"
 import Layout from './components/Layout/Layout';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 function App() {
+  moment.locale("pt-br")
   return (
     <Layout>
       <Router/>
