721272043edd5d0468fef0ae0d9f0f0f42890d72
Eduardo <luiz.dudu3@gmail.com>
2022-07-08 16:17:09 -0300

    populate teachers on get user

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,12 +2,12 @@ import { Request, Response } from 'express';
 import User from '@models/user';
 import IUser from '@interfaces/user';
 import HttpError from '@models/errors/HttpError';
-import CrudController from './crudController';
-import mongoose from '../database';
-import Role from '../enums/role';
 import { ParamsDictionary } from 'express-serve-static-core';
 import { ParsedQs } from 'qs';
 import Turma from '@models/turma';
+import CrudController from './crudController';
+import mongoose from '../database';
+import Role from '../enums/role';
 
 require('express-async-errors');
 
@@ -17,7 +17,7 @@ class UserController extends CrudController<IUser, typeof User> {
   }
 
   override populate(entity) {
-    return entity.populate("wantedTurmas");
+    return entity.populate('wantedTurmas');
   }
 
   override async createFromParameters(request: Request): Promise<IUser> {
@@ -41,75 +41,74 @@ class UserController extends CrudController<IUser, typeof User> {
   }
 
   override prepareQuery(request: Request, query: mongoose.FilterQuery<IUser>, options: any): void {
-    options.populate = ['wantedTurmas']
+    options.populate = ['wantedTurmas'];
 
-    const {name, email, role, status} = request.query
+    const {
+      name, email, role, status,
+    } = request.query;
     if (name) {
-      query.name = {$regex: new RegExp(name as string), $options: "i"}
+      query.name = { $regex: new RegExp(name as string), $options: 'i' };
     }
     if (email) {
-      query.email = {$regex: new RegExp(email as string), $options: "i"}
+      query.email = { $regex: new RegExp(email as string), $options: 'i' };
     }
     if (role) {
-      query.role = role
+      query.role = role;
     }
     if (status) {
-      query.status = status
+      query.status = status;
     }
   }
 
   override async posUpdate(user: IUser, request: Request): Promise<void> {
     // recebe as turmas e adiciona o usuario na turma
-    const {turmas: novasTurmas} = request.body
-    if (!novasTurmas) return
-    const userId = user._id
-    const roleField = user.role === 'teacher' || user.role === 'admin' ? 'teachers' : 'students'
+    const { turmas: novasTurmas } = request.body;
+    if (!novasTurmas) return;
+    const userId = user._id;
+    const roleField = user.role === 'teacher' || user.role === 'admin' ? 'teachers' : 'students';
     await Turma.updateMany(
       { [roleField]: userId },
-      { "$pull": { [roleField]: userId } },
-      { "multi": true },
-    )
-    await Turma.updateMany(
-      { "_id": { "$in": novasTurmas } },
-      { "$addToSet": { [roleField]: userId } },
-      { "multi": true },
+      { $pull: { [roleField]: userId } },
+      { multi: true },
     );
-   
+    await Turma.updateMany(
+      { _id: { $in: novasTurmas } },
+      { $addToSet: { [roleField]: userId } },
+      { multi: true },
+    );
   }
 
   override async posRead(user: IUser): Promise<void> {
-    const roleField = user.role === 'teacher' || user.role === 'admin' ? 'teachers' : 'students'
-    const turmas = await Turma.find({[roleField]: user._id}).populate("disciplina").populate("aulas").exec()
-    user._doc.turmas = turmas
+    const roleField = user.role === 'teacher' || user.role === 'admin' ? 'teachers' : 'students';
+    const turmas = await Turma.find({ [roleField]: user._id }).populate('disciplina').populate('aulas').populate('teachers')
+      .exec();
+    user._doc.turmas = turmas;
   }
- 
+
   professoras = async (request: Request, response: Response): Promise<Response> => {
-    const pesquisa = this.pesquisaPadrao(request)
+    const pesquisa = this.pesquisaPadrao(request);
     pesquisa[0] = {
       ...pesquisa[0],
       $or: [
-        { 'role': 'admin' },
-        { 'role': 'teacher' }
-      ]
-    }
-    const result = await this.getEntity().paginate(...pesquisa)
+        { role: 'admin' },
+        { role: 'teacher' },
+      ],
+    };
+    const result = await this.getEntity().paginate(...pesquisa);
 
     return response.status(200).json({ data: result.docs, total: result.totalPages });
   };
 
   alunas = async (request: Request, response: Response): Promise<Response> => {
-    const pesquisa = this.pesquisaPadrao(request)
+    const pesquisa = this.pesquisaPadrao(request);
     pesquisa[0] = {
       ...pesquisa[0],
-      role: 'student'
-    }
-    const result = await this.getEntity().paginate(...pesquisa)
+      role: 'student',
+    };
+    const result = await this.getEntity().paginate(...pesquisa);
 
     return response.status(200).json({ data: result.docs, total: result.totalPages });
   };
-
-
-
 }
 
 export default new UserController();
