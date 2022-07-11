6a29428956c8a5114a1a9612c3eee34774ac2799
Eduardo <luiz.dudu3@gmail.com>
2022-07-11 10:28:27 -0300

    add finalizar turma route

--- backend/src/routes/turmaRoutes.ts
+++ backend/src/routes/turmaRoutes.ts
@@ -10,6 +10,7 @@ router.get('/', turmaController.list);
 router.get('/:id', turmaController.read);
 router.use(authMiddleware);
 router.put('/:id', roleMiddleware([Role.TEACHER]), turmaController.update);
+router.post('/finalizar/:id', roleMiddleware([Role.TEACHER]), turmaController.finalizar);
 router.delete('/:id', roleMiddleware([Role.TEACHER]), turmaController.delete);
 router.post('/', roleMiddleware([Role.TEACHER]), turmaController.create);
 
